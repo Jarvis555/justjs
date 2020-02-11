4903eb60cda7f0a670978b5125bf1caca3702871
Jarvis555 <jarvisjackson591@gmail.com>
2020-02-11 09:42:09 -0600

    .

--- volcanoes.js
+++ volcanoes.js
@@ -1,37 +1,39 @@
 let volcanoes = require('./volcanoes.json');
-// eruptedIn70s();
+ eruptedIn70s();
+volcanoes.forEach(v => v.DEATHS = +v.DEATHS );
+function eruptedIn70s() {
+    for (const vol of volcanoes) {
+        if (vol.Year < 0) {
+            console.table(vol);
+        }
 
-// function eruptedIn70s() {
-//     for (const vol of volcanoes) {
-//         if (vol.Year < 0) {
-//             console.table(vol);
-//         }
+        if (vol.Location == "Italy") {
+            console.table(vol);
+        }
 
-//         if (vol.Location == "Italy") {
-//             console.table(vol);
-//         }
+    }
 
-//     }
+}
+let volCount = 0;
+let volDeath = 0;
+let mostDeath = volcanoes[0];
+for (const vol of volcanoes) {
+    if (vol.Year > 1988 && vol.Year < 2020) {
+        volCount++;
+    }
+    if (vol.DEATHS > 0) {
+        volDeath += vol.DEATHS;
+    }
+    if (mostDeath.DEATHS < vol.DEATHS) {
+        mostDeath = vol;
+    }
 
-// }
-// let volCount = 0;
-// let volDeath = 0;
-// let mostDeath = volcanoes[0];
-// for (const vol of volcanoes) {
-//     if (vol.Year > 1988 && vol.Year < 2020) {
-//         volCount++;
-//     }
-//     if (vol.DEATHS > 0) {
-//         volDeath += vol.DEATHS;
-//     }
-//     if (mostDeath.DEATHS < vol.DEATHS) {
-//         mostDeath = vol;
-//     }
+}
+console.log(`There is ${volCount} between 1988 and 2020.`);
+console.log(`There is ${volDeath} total deaths by volcanoes.`);
+console.table(mostDeath);
 
-// }
-// console.log(`There is ${volCount} between 1988 and 2020.`);
-// console.log(`There is ${volDeath} total deaths by volcanoes.`);
-// console.table(mostDeath);
+
 let countheight = countOfHighestPeak(1000);
 console.log(`There are ${countheight} volcanoes over 1000ft.`);
 countheight = countOfHighestPeak(5000);
@@ -61,3 +63,14 @@ function listOfCounty(country) {
     }
     return listOfVolcanoes;
 }
+let totalDeaths = volcanoes.reduce ((total, v) => total + v.DEATHS, 0);
+console.table('number of deaths ' + totalDeaths);
+
+let totalElevation = volcanoes.reduce ((total, v) => total + v.Elevation, 0);
+console.table('number of elevation ' + totalElevation);
+
+let average = (totalElevation / volcanoes.length);
+console.log(average);
+
+average = (totalDeaths / volcanoes.length);
+console.log(average);
